40da1734506ebac0a44e9a8f9574461a1fa63c5d
Fox Dana <teviccst@gmail.com>
2026-07-04 22:40:54 +0500

    create components button and input

---
+++ src/components/UI/Input/Input.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './Input.module.css'
+
+interface InputProps{
+    value?: string;
+    onChange?: (value: string) => void;
+    placeholder?: string;
+    disabled?: boolean
+}
+
+export const Input = ({
+    value,
+    onChange,
+    placeholder = 'input',
+    disabled = false
+}: InputProps) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (onChange) {
+            onChange(e.target.value)
+        }
+    };
+
+    return(
+        <input
+        type="text"
+        className={styles.input}
+        onChange={handleChange}
+        value={value}
+        placeholder={placeholder}
+        disabled={disabled}
+        />
+    )
+}
