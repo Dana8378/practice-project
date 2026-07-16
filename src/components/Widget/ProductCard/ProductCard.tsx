45e4f959851bba6aaedd2953ddb33c371880eb03
Fox Dana <teviccst@gmail.com>
2026-07-16 13:41:15 +0500

    ProductCard have been created

---
+++ src/components/Widget/ProductCard/ProductCard.tsx
@@ -0,0 +1,38 @@
+import { Card } from "../../UI/Card"
+import { Button } from "../../UI/Button"
+import { Link } from "react-router-dom"
+import { useAuth } from "../../../context/AuthContext"
+import type { Product } from "../../../data/products"
+import styles from "./ProductCard.module.css"
+
+interface ProductCardProps {
+    product: Product;
+}
+
+export const ProductCard = ({product}: ProductCardProps) => {
+    const { isAuthenticated } = useAuth();
+
+    const formattedPrice = product.price.toLocaleString('ru-RU');
+
+    const handleAddToCard = () => {
+        alert(`${product.name} добавлен в корзину`)
+    };
+
+    return(
+        <Card 
+            title={product.name}
+            description={`${formattedPrice} руб.`}
+            imageUrl={product.image}
+        >
+            <div className={styles.actions}>
+                <Link to={`/product/${product.id}`} className={styles.detailLink}>Подробнее</Link>
+
+                {isAuthenticated ? (
+                    <Button onClick={handleAddToCard}>В корзину</Button>
+                ) : (
+                    <Link to={"/login"} className={styles.loginLink}>Войдите, чтобы купить</Link>
+                )}
+            </div>
+        </Card>
+    )
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,16 @@
+import { products } from "../../data/products"
+import { ProductCard } from "../../components/Widget/ProductCard"
 import styles from "./Home.module.css"
 
 export const Home = () => {
     return (
         <div className={styles.page}>
-            <h1>Главная страница</h1>
-            <p>Здесь будет каталог</p>
+            <h1 className={styles.title}>Каталог цветов</h1>
+            <div className={styles.catalog}>
+                {products.map(product => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+            </div>
         </div>
     )
 }
